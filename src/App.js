1dd95f6627549074c85faba28aef359254bf2beb
Kasia <kasia.ekiert@gmail.com>
2019-12-28 10:27:25 +0100

    refactoring to funcional programming

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import Users from "./components/users/Users";
 import Navbar from "./components/layout/Navbar";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users")
+      .then(res => console.log(res.data));
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <h1>Hello</h1>
+        <div className='container'>
+          <Users />
+          <h1>Hello</h1>
+        </div>
       </div>
     );
   }
